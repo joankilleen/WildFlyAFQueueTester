065d2d515977c289c073882daaaa95da3f75f201
Joan Killeen <joan.killeen@bluewin.ch>
2018-01-01 08:29:50 +0100

    first commit

---
+++ src/main/java/com/killeen/WildFlyAFQueueTester/App.java
@@ -0,0 +1,13 @@
+package com.killeen.WildFlyAFQueueTester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
